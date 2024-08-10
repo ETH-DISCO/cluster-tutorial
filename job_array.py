2285b5da3d4fdc788ca42bdcac864bb79b0fcdb3
yahya jabary <61852663+sueszli@users.noreply.github.com>
2024-08-10 18:25:58 +0200

    up

--- job_array.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("arg1", type=int, help="An argument", required=True)
-args = parser.parse_args()
-
-print("You successfully ran an array job with argument:", args.arg1)
