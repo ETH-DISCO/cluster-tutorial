2836e5757d7321c6d0ecf39b67106a3d660b6a76
sueszli <jabaryyahya@gmail.com>
2024-08-11 16:10:18 +0200

    up

--- job_array.py
+++ job_array.py
@@ -92,7 +92,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
+        if batch_idx % args["log_interval"] == 0:
             print("Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(epoch, batch_idx * len(data), len(train_loader.dataset), 100.0 * batch_idx / len(train_loader), loss.item()))
 
 
@@ -126,18 +126,18 @@ def main():
         "log_interval": 25,
     }
 
-    torch.manual_seed(args.seed)
+    torch.manual_seed(args["seed"])
 
     kwargs = {"num_workers": 1, "pin_memory": True}
-    train_loader = torch.utils.data.DataLoader(datasets.MNIST("./data", train=True, download=True, transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])), batch_size=args.batch_size, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(datasets.MNIST("./data", train=False, transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])), batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    train_loader = torch.utils.data.DataLoader(datasets.MNIST("./data", train=True, download=True, transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])), batch_size=args["batch_size"], shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(datasets.MNIST("./data", train=False, transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])), batch_size=args["test_batch_size"], shuffle=True, **kwargs)
 
     device = get_device()
     model = Net().to(device)
-    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+    optimizer = optim.Adadelta(model.parameters(), lr=args["lr"])
 
-    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for epoch in range(1, args.epochs + 1):
+    scheduler = StepLR(optimizer, step_size=1, gamma=args["gamma"])
+    for epoch in range(1, args["epochs"] + 1):
         train(args, model, device, train_loader, optimizer, epoch)
         test(args, model, device, test_loader)
         scheduler.step()
