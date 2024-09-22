131a9e1f6fe084a658aa458dc429d9b5960a981b
yahya jabary <61852663+sueszli@users.noreply.github.com>
2024-09-22 21:19:48 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mail-type=NONE # disable email notifications can be [NONE, BEGIN, END, FAIL, REQUEUE, ALL]
-#SBATCH --output=/itet-stor/{{USERNAME}}/net_scratch/demo/jobs/%j.out # redirection of stdout (%j is the job id)
-#SBATCH --error=/itet-stor/{{USERNAME}}/net_scratch/demo/jobs/%j.err # redirection of stderr
+#SBATCH --output=/itet-stor/{{USERNAME}}/net_scratch/slurm/output/%j.out # redirection of stdout (%j is the job id)
+#SBATCH --error=/itet-stor/{{USERNAME}}/net_scratch/slurm/output/%j.err # redirection of stderr
 #SBATCH --mem=100G
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
@@ -12,18 +12,16 @@
 #CommentSBATCH --constraint='titan_rtx|tesla_v100|titan_xp|a100_80gb' # example: specify a gpu
 
 set -o errexit # exit on error
+rm -rf /itet-stor/${USER}/net_scratch/slurm/output
+mkdir -p /itet-stor/${USER}/net_scratch/slurm/output
 
-DIRECTORY=/itet-stor/${USER}/net_scratch/demo
-CONDA_ENVIRONMENT=demo
-mkdir -p /itet-stor/${USER}/net_scratch/demo/jobs
-
-TMPDIR=$(mktemp -d)
+TMPDIR=$(mktemp -d) # create a temp directory for the job
 if [[ ! -d ${TMPDIR} ]]; then
   echo 'Failed to create temp directory' >&2
   exit 1
 fi
-trap "exit 1" HUP INT TERM
-trap 'rm -rf "${TMPDIR}"' EXIT
+trap "exit 1" HUP INT TERM # exit on interrupt
+trap 'rm -rf "${TMPDIR}"' EXIT # cleanup on exit
 export TMPDIR
 cd "${TMPDIR}"
 
@@ -32,16 +30,12 @@ echo "in directory: $(pwd)"
 echo "starting on: $(date)"
 echo "SLURM_JOB_ID: ${SLURM_JOB_ID}"
 
-# -----
-
 [[ -f /itet-stor/${USER}/net_scratch/conda/bin/conda ]] && eval "$(/itet-stor/${USER}/net_scratch/conda/bin/conda shell.bash hook)"
-conda activate ${CONDA_ENVIRONMENT}
-echo "Conda activated"
-cd ${DIRECTORY}
 
-python job.py
-
-# -----
+# ---------------- run the job ----------------
+conda activate con
+python mnist.py
+# ---------------------------------------------
 
 echo "finished at: $(date)"
 exit 0
