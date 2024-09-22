118a150e2640b65f8fad12245a48b2b4dcaf2d55
yahya jabary <61852663+sueszli@users.noreply.github.com>
2024-09-22 21:06:08 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mail-type=NONE # mail configuration: NONE, BEGIN, END, FAIL, REQUEUE, ALL
-#SBATCH --output=/itet-stor/{{USERNAME}}/net_scratch/cluster/jobs/%j.out # where to store the output (%j is the JOBID), subdirectories must exist
-#SBATCH --error=/itet-stor/{{USERNAME}}/net_scratch/cluster/jobs/%j.err # where to store error messages
+#SBATCH --output=/itet-stor/{{USERNAME}}/net_scratch/demo/jobs/%j.out # where to store the output (%j is the JOBID), subdirectories must exist
+#SBATCH --error=/itet-stor/{{USERNAME}}/net_scratch/demo/jobs/%j.err # where to store error messages
 #SBATCH --mem=20G
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
@@ -11,36 +11,31 @@
 #CommentSBATCH --account=tik-internal
 #CommentSBATCH --constraint='titan_rtx|tesla_v100|titan_xp|a100_80gb'
 
+set -o errexit # exit on error
+
 # ------------------------------------------ end of configuration ------------------------------------------
 
 DIRECTORY=/itet-stor/${USER}/net_scratch/demo
 CONDA_ENVIRONMENT=demo
-mkdir -p ${DIRECTORY}/jobs
-
-# Exit on errors
-set -o errexit
+mkdir -p /itet-stor/${USER}/net_scratch/demo/jobs
 
 # Set a directory for temporary files unique to the job with automatic removal at job termination
 TMPDIR=$(mktemp -d)
 if [[ ! -d ${TMPDIR} ]]; then
-echo 'Failed to create temp directory' >&2
-exit 1
+  echo 'Failed to create temp directory' >&2
+  exit 1
 fi
 trap "exit 1" HUP INT TERM
 trap 'rm -rf "${TMPDIR}"' EXIT
 export TMPDIR
+cd "${TMPDIR}"
 
-# Change the current directory to the location where you want to store temporary files, exit if changing didn't succeed.
-# Adapt this to your personal preference
-cd "${TMPDIR}" || exit 1
-
-# Send some noteworthy information to the output log
+# log some stuff
 
 echo "running on node: $(hostname)"
 echo "in directory: $(pwd)"
 echo "starting on: $(date)"
 echo "SLURM_JOB_ID: ${SLURM_JOB_ID}"
-
 
 [[ -f /itet-stor/${USER}/net_scratch/conda/bin/conda ]] && eval "$(/itet-stor/${USER}/net_scratch/conda/bin/conda shell.bash hook)"
 conda activate ${CONDA_ENVIRONMENT}
