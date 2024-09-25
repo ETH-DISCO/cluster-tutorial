cae8daedb6d3050179684968f040e495b19c8a65
Yahya Jabary <61852663+sueszli@users.noreply.github.com>
2024-09-25 23:42:02 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -2,12 +2,12 @@
 #SBATCH --mail-type=NONE # disable email notifications can be [NONE, BEGIN, END, FAIL, REQUEUE, ALL]
 #SBATCH --output=/itet-stor/{{USERNAME}}/net_scratch/slurm/%j.out # redirection of stdout (%j is the job id)
 #SBATCH --error=/itet-stor/{{USERNAME}}/net_scratch/slurm/%j.err # redirection of stderr
+#SBATCH --nodelist={{NODE}} # choose specific node
 #SBATCH --mem=100G
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 #SBATCH --exclude=tikgpu[08-10]
-#CommentSBATCH --nodelist=tikgpu01 # example: specify a node
 #CommentSBATCH --account=tik-internal # example: charge a specific account
 #CommentSBATCH --constraint='titan_rtx|tesla_v100|titan_xp|a100_80gb' # example: specify a gpu
 
